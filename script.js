665b5606e167342e7f05d90fc8c2bdaf684a1600
AmanDwivedi2324 <0786ad2000@gmail.com>
2024-09-16 22:01:18 +0530

    Update files

--- script.js
+++ script.js
@@ -144,3 +144,17 @@ page4Animation();
 //     svgTxt.style.transform = "scale(0)";
     
 // })
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
